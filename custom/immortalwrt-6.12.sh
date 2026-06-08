1ee73676ac88cb72f0d214c7106cb0e5b2caed6b
0118Add <60157610+0118Add@users.noreply.github.com>
2026-06-08 17:15:18 +0800

    Update immortalwrt-6.12.sh

--- custom/immortalwrt-6.12.sh
+++ custom/immortalwrt-6.12.sh
@@ -51,7 +51,8 @@ git_sparse_clone main https://github.com/0118Add/Openwrt-CI autocore
 git clone https://github.com/sbwml/default-settings package/default-settings
 
 # 核心库
-rm -rf feeds/packages/net/{dae,xray-core}
+rm -rf feeds/luci/applications/{luci-app-dae,luci-app-daed}
+rm -rf feeds/packages/net/{dae,daed,xray-core}
 git clone https://github.com/Openwrt-Passwall/openwrt-passwall-packages package/passwall-packages
 
 # openclash
@@ -71,12 +72,13 @@ sed -i "s/ImmortalWrt proxy/OpenWrt proxy/g" feeds/luci/applications/luci-app-ho
 #git clone -b main --depth 1 https://github.com/nikkinikki-org/OpenWrt-momo package/OpenWrt-momo
 
 # dae
-rm -rf feeds/luci/applications/{luci-app-dae,luci-app-daed}
-rm -rf feeds/packages/net/{dae,daed}
-git_sparse_clone kix https://github.com/QiuSimons/luci-app-dae luci-app-dae
-git_sparse_clone master https://github.com/8688Add/openwrt_pkgs dae
-sed -i 's/PKG_VERSION:=.*/PKG_VERSION:=2026.06.05/g' package/dae/Makefile
-sed -i 's/PKG_SOURCE_VERSION:=.*/PKG_SOURCE_VERSION:=ba50380e15649b0c83ec04c07f355dece19b0cea/g' package/dae/Makefile
+#git_sparse_clone kix https://github.com/QiuSimons/luci-app-dae luci-app-dae
+#git_sparse_clone master https://github.com/8688Add/openwrt_pkgs dae
+#sed -i 's/PKG_VERSION:=.*/PKG_VERSION:=2026.06.05/g' package/dae/Makefile
+#sed -i 's/PKG_SOURCE_VERSION:=.*/PKG_SOURCE_VERSION:=ba50380e15649b0c83ec04c07f355dece19b0cea/g' package/dae/Makefile
+git_sparse_clone master https://github.com/QiuSimons/OpenWrt-Add luci-app-dae openwrt-einat-ebpf
+sed -i 's/+@KERNEL_DEBUG_INFO_BTF/+vmlinux-btf/' package/openwrt-einat-ebpf/Makefile
+git clone https://github.com/QiuSimons/vmlinux-btf package/vmlinux-btf
 
 # partexp
 git clone https://github.com/sirpdboy/luci-app-partexp package/luci-app-partexp
