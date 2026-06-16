f4cfc212d3c1e59a05ddf21a6a681bb17ddde179
0118Add <60157610+0118Add@users.noreply.github.com>
2026-06-16 13:04:14 +0800

    Clean up script and update video feed source
    Remove commented lines for hostname and default settings modification, and update video feed source.

--- custom/IPQ60XX-6.12.sh
+++ custom/IPQ60XX-6.12.sh
@@ -19,9 +19,7 @@ function git_sparse_clone() {
   cd .. && rm -rf $repodir
 }
 
-# 修改主机名字（不能纯数字或者使用中文）
-#sed -i "s/hostname='.*'/hostname='X86'/g" package/base-files/files/bin/config_generate
-#sed -i "s/OpenWrt /OPWRT/g" package/lean/default-settings/files/zzz-default-settings
+sed -i '# src-git video https://github.com/openwrt/video' feeds.conf.default
 
 # 修改默认IP
 sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
