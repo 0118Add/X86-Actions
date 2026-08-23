378e476d43374264d48e47761a138e2c5db7c996
0118Add <60157610+0118Add@users.noreply.github.com>
2026-08-23 15:10:23 +0800

    Fix paths for dockerd patches in Lienol-Test.sh

--- custom/Lienol-Test.sh
+++ custom/Lienol-Test.sh
@@ -338,9 +338,8 @@ sed -i 's/"admin/"admin\/services/g' feeds/lienol/other/luci-app-dockerman/luasr
 sed -i 's/"admin/"admin\/services/g' feeds/lienol/other/luci-app-dockerman/luasrc/model/cbi/dockerman/*.lua
 sed -i 's/"admin/"admin\/services/g' feeds/lienol/other/luci-app-dockerman/luasrc/view/dockerman/*.htm
 sed -i 's/"admin/"admin\/services/g' feeds/lienol/other/luci-app-dockerman/luasrc/view/dockerman/cbi/*.htm
-#mkdir -p feeds/packages/utils/dockerd/patches
-mkdir -p package/feeds/packages/dockerd
-curl -s https://raw.githubusercontent.com/0118Add/X86_64-Test/main/general/patches/001-skip-copy-nested-binaries.patch > package/feeds/packages/dockerd/patches/001-skip-copy-nested-binaries.patch
+mkdir -p feeds/packages/utils/dockerd/patches
+curl -s https://raw.githubusercontent.com/0118Add/X86_64-Test/main/general/patches/001-skip-copy-nested-binaries.patch > feeds/packages/utils/dockerd/patches/001-skip-copy-nested-binaries.patch
 
 # 调整 tailscale zerotier 到 服务 菜单
 #sed -i 's/vpn/services/g' feeds/luci/applications/luci-app-zerotier/root/usr/share/luci/menu.d/luci-app-zerotier.json
